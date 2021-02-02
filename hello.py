0c3aa3ed68ab631da665f6d9ab1d5ab1dd801572
ccravo <77801385+ccravo@users.noreply.github.com>
2021-02-02 00:48:50 +0000

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,12 +1,1 @@
-import math
-print(math.tan(90))
-a = int(input('Qual o valor de a? '))
-b = int(input('qual o valor de b? '))
-
-
-def adicao(a, b):
-	y = a + b
-	return y
-
-
-print(adicao(a, b))
+print("Olá a todos os pythonistas portugueses!")
