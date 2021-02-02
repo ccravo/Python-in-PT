22d56243a6deac5086b3be0ae0efaba8bfc33142
ccravo <77801385+ccravo@users.noreply.github.com>
2021-02-02 00:46:59 +0000

    Add files via upload

---
+++ caracol_turtle.py
@@ -0,0 +1,12 @@
+import turtle
+
+turtle.bgcolor('black')
+turtle.pencolor('white')
+turtle.speed(10)
+for i in range(1):
+	for j in range(100):
+		turtle.circle(j)
+		turtle.left(10)
+
+
+turtle.done()
