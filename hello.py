397f47bac1b4d85240466041586fc73516848adf
ccravo <77801385+ccravo@users.noreply.github.com>
2021-02-02 00:40:49 +0000

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,12 @@
+import math
+print(math.tan(90))
+a = int(input('Qual o valor de a? '))
+b = int(input('qual o valor de b? '))
+
+
+def adicao(a, b):
+	y = a + b
+	return y
+
+
+print(adicao(a, b))
